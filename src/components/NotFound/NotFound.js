355ef8857c7e9a7bf7533dc86a70e2eaebf527b0
jerinira <jerin00177@gmail.com>
2020-03-19 10:50:10 +0100

    finished coding

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+                <h1>Sorry, page not Found</h1>
+                <h2>404 ERROR!!!</h2>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/components/ReviewItem/ReviewItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ReviewItem = (props) => {
+    console.log(props);
+    const {name,quantity,key,price}=props.product;
+    const reviewItemStyle={
+        borderBottom: '1px solid gray',
+        marginBottom:'5x',
+        paddingBottom:'5px',
+        marginLeft:'200px'
+    };
+    return (
+        <div style={reviewItemStyle}>
+            <h4 className="product-name">{name}</h4>
+            <p>Quantity {quantity}</p>
+            <p>Price ${price}</p>
+            <br/>
+            <button onClick={()=>props.removeProduct(key)} className="main-button">Review Item</button>
+        </div>
+    );
+};
+
+export default ReviewItem;
